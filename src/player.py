bb49aebdf269ef402ed002fe8983c709f9931215
Ethret <Ethret@users.noreply.github.com>
2020-07-16 21:31:19 -0500

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,6 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Explorer:
-    def __init__(self, name, room):
+    def __init__(self, name, room, inventory=[]):
         self.name = name
         self.room = room
+        self.inventory = inventory
+
+player = Explorer("Adric", "Outside")
+print(player.inventory)
